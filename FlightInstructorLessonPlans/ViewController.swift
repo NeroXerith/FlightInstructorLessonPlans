5761b097dfd4691b31119eafaeffe9409c98eb9e
NeroXerith <astar8820@gmail.com>
2025-06-08 15:44:13 +0800

    Initial Commit

---
+++ FlightInstructorLessonPlans/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlightInstructorLessonPlans
+//
+//  Created by Biene Bryle Sanico on 6/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
