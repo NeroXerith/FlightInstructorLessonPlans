0e3330d58ffc663a05dff1b123033ccfc9a1bc62
NeroXerith <astar8820@gmail.com>
2025-06-08 17:08:22 +0800

    String Test Case

--- FlightInstructorLessonPlans/README.md
+++ FlightInstructorLessonPlans/README.md
@@ -6,11 +6,23 @@ As a flight instructor I want to be able to customize a lesson plan for a studen
 From a list of flight maneuvers I can select different ones to add to today's lesson plan. When finished with the selection I can press the done button and be taken to a different page I can share with my student.
 
 ### Steps
-1. Get all flight maneuvers (From an API/Backend)
-2. Display them on screen (list of maneuver names)
-3. User selects specific maneuvers
-4. User hits done button
-5. Selected flight maneuvers are saved locally
-6. Selected flight maneuvers are displayed on new screen (with any additional information)
-7. User can go back and make changes as needed
+* Get all flight maneuvers (From an API/Backend)
+* Display them on screen (list of maneuver names)
+* User selects specific maneuvers
+* User hits done button
+* Selected flight maneuvers are saved locally
+* Selected flight maneuvers are displayed on new screen (with any additional information)
+* User can go back and make changes as needed
+
+## Manuevers Selector UI Checklist
+- Show a list of names (0, 1, many)
+- Select manuever
+
+## Details Manuevers UI Checklist
+- Shows a list of maneuvers
+    - Name
+    - Image
+    - Description
+    - ...
+    
 
--- FlightInstructorLessonPlansTests/FlightInstructorLessonPlansTests.swift
+++ FlightInstructorLessonPlansTests/FlightInstructorLessonPlansTests.swift
@@ -8,29 +8,31 @@
 import XCTest
 @testable import FlightInstructorLessonPlans
 
+struct Maneuver {
+    let name, description: String
+}
+
+class ListOfManueverSelectorViewController: UITableViewController {
+    convenience init(items: [String]) {
+        self.init()
+    }
+}
+
 final class FlightInstructorLessonPlansTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_emptyList(){
+        // given
+        // let maneuver = [Maneuver]()
+        let items = [String]()
+        let sut = ListOfManueverSelectorViewController(items: items)
+        
+        // when
+            // we load the view
+        sut.loadViewIfNeeded()
+        
+        // then
+            // we should have an empty list
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
     }
 
 }
