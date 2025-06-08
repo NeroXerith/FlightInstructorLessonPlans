a3fb4ca59634ae2771d47534128d30730f4ac3c9
NeroXerith <astar8820@gmail.com>
2025-06-08 17:38:55 +0800

    Unit test (2 test cases)

--- FlightInstructorLessonPlansTests/FlightInstructorLessonPlansTests.swift
+++ FlightInstructorLessonPlansTests/FlightInstructorLessonPlansTests.swift
@@ -13,8 +13,13 @@ struct Maneuver {
 }
 
 class ListOfManueverSelectorViewController: UITableViewController {
-    convenience init(items: [String]) {
-        self.init()
+    private var items: [Maneuver] = []
+    func display(items: [Maneuver]) {
+        self.items = items
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
     }
 }
 
@@ -23,8 +28,7 @@ final class FlightInstructorLessonPlansTests: XCTestCase {
     func test_emptyList(){
         // given
         // let maneuver = [Maneuver]()
-        let items = [String]()
-        let sut = ListOfManueverSelectorViewController(items: items)
+        let sut = makeSUT(items: [])
         
         // when
             // we load the view
@@ -33,6 +37,21 @@ final class FlightInstructorLessonPlansTests: XCTestCase {
         // then
             // we should have an empty list
         XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 0)
+    }
+    
+    // Red->Green->Refactor
+    func test_oneItem(){
+        let sut = makeSUT(items: ["test"]) // Added 1 element to the Maneuvers
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(sut.tableView.numberOfRows(inSection: 0), 1)
+    }
+    
+    private func makeSUT(items: [String]) -> ListOfManueverSelectorViewController {
+        let sut = ListOfManueverSelectorViewController()
+        sut.display(items: items.map { Maneuver(name: $0, description: $0)})
+        return sut
     }
 
 }
